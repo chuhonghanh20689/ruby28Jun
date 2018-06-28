2273f7471bbd1b55e97909196fa2752a33c7eea3
hanhch <chuhonghanh20689@gmail.com>
2018-06-28 10:55:47 +0700

    finish chap 6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    def full_title(base_title = '')
+        return "#{base_title}" if base_title.empty?
+        return "#{base_title} | Ruby training"
+    end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get root_path
     assert_response :success
+    assert_template "users/new"
+    assert_select "title", "Home | #{@subtitle}"
+    assert_select "a[href=?]", root_path
   end
 
 end
